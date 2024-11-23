e87c8d6ebb99221b83365774d2a5ccabe7de3bcd
Wesley <wmiller1089@gmail.com>
2024-11-22 18:50:45 -0700

    - graph and subgraph build method, checks SET for excluded vertices to skip adding edges to - basic rectilinear operations with new record `Point` - move old java files to old/

--- src/autoRouter/Display.java
+++ src/autoRouter/Display.java
@@ -9,158 +9,122 @@ import java.awt.*;
 /// TODO add listeners for mouse events, Get position of mouse on gridmap during click -> add location to node list.
 /// TODO drop-down menu for some predefined examples
 /// TODO button to generate random nodes
-/// TODO nametags for nodes or unique node colors with legend
-public class Display implements Runnable{
+/// TODO Associate circles with nametags.  Either write a nametag placement method or uniquely color circle with a legend
+public class Display {
 
-        Draw pane;
+    Draw pane;
 
     /// Displays information provided by the Plane object (node positions as circles, edges as lines)
 
-        static final int width = 300; static final int height = 300;
-        static final double gridCount = SquareGrid.squares;
-        static final double unit = 1 / gridCount;   // dimensions of unit square , gives a 20 x 20 grid
-        static double pointDiameter = 0.5;          // point size
+    static final int width = 300;
+    static final int height = 300;
+    static final double gridCount = 10/*SquareGrid.squares*/;
+    static final double unit = 1 / gridCount;   // dimensions of unit square , gives a 20 x 20 grid
+    static double pointDiameter = 0.5;          // point size
 
-        public static void grid(Draw pane){
-            grid(width, height, unit , pane );
-        }
-        /// Draw a grid, 20x20
-        private static void grid(int width, int height, double unit, Draw pane){
-            pane.setPenColor(Draw.LIGHT_GRAY);
-            for (int x = 0; x < width; x ++){
-                pane.setPenRadius((x % 5 == 0) ? 0.002 : 0.0005);
-                double xTick = x * unit;
-                pane.line(xTick, 0.0,xTick,1.0);
-            }
-            for (int y = 0; y < height; y ++){
-                pane.setPenRadius((y % 5 == 0) ? 0.002 : 0.0005);
-                double yTick = y * unit;
-                pane.line(0.0, yTick,1.0,yTick);
-            }
-            pane.setPenColor(Draw.BLACK);
-        }
+    public static void grid(Draw pane) {
+        grid(width, height, unit, pane);
+    }
 
-        private static void twoPointsInline_displayTest(Draw pane) {
-            Bag<Int2D> points = new Bag<>();
-            Int2D a = new Int2D(1,1);
-            Int2D b = new Int2D(1,3);
-            Int2D m = new Int2D(1,2);
+    /// Draw a grid, 20x20
+    private static void grid(int width, int height, double unit, Draw pane) {
+        pane.setPenColor(Draw.LIGHT_GRAY);
+        for (int x = 0; x < width; x++) {
+            pane.setPenRadius((x % 5 == 0) ? 0.002 : 0.0005);
+            double xTick = x * unit;
+            pane.line(xTick, 0.0, xTick, 1.0);
+        }
+        for (int y = 0; y < height; y++) {
+            pane.setPenRadius((y % 5 == 0) ? 0.002 : 0.0005);
+            double yTick = y * unit;
+            pane.line(0.0, yTick, 1.0, yTick);
+        }
+        pane.setPenColor(Draw.BLACK);
+    }
 
-            points.add(a);
-            points.add(b);
-            drawCirclesMini(points, pane);
-            drawPoint(m, pane);
-        }
-        // TODO Associate circles with names. write either a nametag placement method or uniquely color circle with a legend
-        public static void drawCircle(Int2D v , Draw pane){
-            pane.setPenColor(Draw.PRINCETON_ORANGE);
-            pane.setPenRadius(0.01);
-            pane.circle(
-                    v.x() * unit,
-                    v.y() * unit,
-                    pointDiameter * unit * 0.5 );
-            pane.setPenRadius();
-        }
-    public static void drawCircle(int x, int y , Color color, Draw pane){
+    public static void drawCircle(int x, int y, Color color, Draw pane) {
         pane.setPenColor(color);
         pane.setPenRadius(0.01);
         pane.circle(
                 x * unit,
                 y * unit,
-                pointDiameter * unit * 0.5 );
+                pointDiameter * unit * 0.5);
         pane.setPenRadius();
     }
-    public static void drawCircle(int x, int y , Draw pane){
+
+    public static void drawCircle(int x, int y, Draw pane) {
         pane.setPenColor(Draw.PRINCETON_ORANGE);
         pane.setPenRadius(0.01);
         pane.circle(
                 x * unit,
                 y * unit,
-                pointDiameter * unit * 0.5 );
+                pointDiameter * unit * 0.5);
         pane.setPenRadius();
     }
-        private static void drawCircle(Int2D v, double diameter, Draw pane){
-            pane.setPenRadius(0.01);
-            pane.circle(
-                    v.x() * unit,
-                    v.y() * unit,
-                    diameter * unit * 0.5);
-            pane.setPenRadius();
-        }
-        // draws a smaller circle
-        public static void drawPoint(Int2D p, Draw pane){
-            double diameter = 0.1;
-            pane.filledCircle(
-                    p.x() * unit,
-                    p.y() * unit,
-                    diameter * unit );
-        }
-    public static void drawPoint(int x, int y, Draw pane){
+
+    public static void drawPoint(int x, int y, Draw pane) {
         double diameter = 0.1;
         pane.filledCircle(
                 x * unit,
                 y * unit,
-                diameter * unit );
+                diameter * unit);
     }
-        public static void drawPoints(Int2D[] points, Draw pane){
-            pane.setPenColor(pane.getPenColor().darker());
-            for(Int2D p : points){
-                drawPoint(p, pane);
-            }
-            pane.setPenColor();
-        }
-        ///  draws points from int[] assumed to be in the form x0, y0 ,x1, y1, ...xi ,yi
-    public static void drawPoints(int[] points, Draw pane){
-        pane.setPenColor(Draw.MAGENTA);
-        for(int i = 1; i< points.length; i+=2){
-                drawPoint(points[i-1],points[i], pane);
-            }
-            pane.setPenColor();
-    }
-    public static void drawCircles(int[] circles, Draw pane){
-        pane.setPenColor(pane.getPenColor().darker());
-        for(int i = 1; i < circles.length; i+=2){
-            drawCircle(circles[i-1],circles[i], pane);
-        }
-        pane.setPenColor();
-    }
-    public static void drawPoints(Iterable<Int2D> points, int msPause, Draw pane) {
-        pane.setPenColor(pane.getPenColor().darker());
-        for (Int2D p : points) {
-            drawPoint(p, pane);
-            pane.show();
-            pane.pause(msPause);
-        }
-        pane.setPenColor();
-    }
-        public static void drawPoint(Int2D p, boolean alt, Draw pane){
-            pane.setPenRadius(0.05);
-            double diameter = 0.1;
-            if(alt){
-                pane.setPenColor(Draw.GREEN);
-                diameter = 0.05;
-            }
-            drawCircle(p, diameter , pane);
-            pane.setPenColor();
-        }
-        private static void drawCirclesMini(Iterable<Int2D> points, Draw pane){
-            points.forEach(p -> drawCircle(p, pane));
-        }
-        public static void drawBlock(Int2D p, Draw pane){
-            pane.setPenColor();
-            pane.square(
-                    p.x() * unit,
-                    p.y() * unit,
-                    0.5* unit
-                    );
-        }
-        public static void init(Draw pane){
-            pane.enableDoubleBuffering();        // defer rendering until show() is called
-            pane.setCanvasSize(width, height);
-            pane.clear(Draw.GRAY);        // set background
-            grid(pane);
-        }
 
+    ///  draws points from int[] assumed to be in the form x0, y0 ,x1, y1, ...xi ,yi
+    public static void drawPoints(int[] points, Draw pane) {
+        pane.setPenColor(Draw.MAGENTA);
+        for (int i = 1; i < points.length; i += 2) {
+            drawPoint(points[i - 1], points[i], pane);
+        }
+        pane.setPenColor();
+    }
+
+    public static void drawCircles(int[] circles, Draw pane) {
+        pane.setPenColor(pane.getPenColor().darker());
+        for (int i = 1; i < circles.length; i += 2) {
+            drawCircle(circles[i - 1], circles[i], pane);
+        }
+        pane.setPenColor();
+    }
+
+    public static void init(Draw pane) {
+        pane.enableDoubleBuffering();        // defer rendering until show() is called
+        pane.setCanvasSize(width, height);
+        pane.clear(Draw.GRAY);        // set background
+        grid(pane);
+    }
+}
+
+
+
+//        public static void drawPoints(Int2D[] points, Draw pane){
+//            pane.setPenColor(pane.getPenColor().darker());
+//            for(Int2D p : points){
+//                drawPoint(p, pane);
+//            }
+//            pane.setPenColor();
+//        }
+    //        private static void drawCircle(Int2D v, double diameter, Draw pane){
+//            pane.setPenRadius(0.01);
+//            pane.circle(
+//                    v.x() * unit,
+//                    v.y() * unit,
+//                    diameter * unit * 0.5);
+//            pane.setPenRadius();
+//        }
+    // draws a smaller circle
+//        public static void drawPoint(Int2D p, Draw pane){
+//            double diameter = 0.1;
+//            pane.filledCircle(
+//                    p.x() * unit,
+//                    p.y() * unit,
+//                    diameter * unit );
+//        }
+//    public static void main(String[] args) {
+//            Draw pane = new Draw();
+//            init(pane);
+// //            twoPointsInline_displayTest(pane);
+//            pane.show();
 //        private static void pause(long ms){
 //            long start = System.currentTimeMillis();
 //            long end = ms + start;
@@ -172,12 +136,37 @@ public class Display implements Runnable{
 //        public static void pause(){
 //            pause(100);
 //        }
-
-        public static void main(String[] args) {
-            Draw pane = new Draw();
-            init(pane);
-            twoPointsInline_displayTest(pane);
-            pane.show();
+//    public static void drawPoints(Iterable<Int2D> points, int msPause, Draw pane) {
+//        pane.setPenColor(pane.getPenColor().darker());
+//        for (Int2D p : points) {
+//            drawPoint(p, pane);
+//            pane.show();
+//            pane.pause(msPause);
+//        }
+//        pane.setPenColor();
+//    }
+//        public static void drawPoint(Int2D p, boolean alt, Draw pane){
+//            pane.setPenRadius(0.05);
+//            double diameter = 0.1;
+//            if(alt){
+//                pane.setPenColor(Draw.GREEN);
+//                diameter = 0.05;
+//            }
+//            drawCircle(p, diameter , pane);
+//            pane.setPenColor();
+//        }
+//        private static void drawCirclesMini(Iterable<Int2D> points, Draw pane){
+//            points.forEach(p -> drawCircle(p, pane));
+//        }
+//        public static void drawBlock(Int2D p, Draw pane){
+//            pane.setPenColor();
+//            pane.square(
+//                    p.x() * unit,
+//                    p.y() * unit,
+//                    0.5* unit
+//                    );
+//        }
+//
 //
 //            Draw paneDebug = new Draw();
 //            init(paneDebug);
@@ -185,13 +174,29 @@ public class Display implements Runnable{
 //            pause();
 //            drawCircle(4,4);
 //            show();
-        }
 
-    @Override
-    public void run() {
+//        private static void twoPointsInline_displayTest(Draw pane) {
+//            Bag<Int2D> points = new Bag<>();
+//            Int2D a = new Int2D(1,1);
+//            Int2D b = new Int2D(1,3);
+//            Int2D m = new Int2D(1,2);
+//
+//            points.add(a);
+//            points.add(b);
+//            drawCirclesMini(points, pane);
+//            drawPoint(m, pane);
+//        }
+//        public static void drawCircle(Int2D v , Draw pane){
+//            pane.setPenColor(Draw.PRINCETON_ORANGE);
+//            pane.setPenRadius(0.01);
+//            pane.circle(
+//                    v.x() * unit,
+//                    v.y() * unit,
+//                    pointDiameter * unit * 0.5 );
+//            pane.setPenRadius();
+//        }
 
-    }
-}
+
 
 
 
